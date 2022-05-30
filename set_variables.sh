e4d12f7167de1462273b289e63ba8ed7b6005714
Kenneth Leine Schulstad <ks@udp.no>
2022-05-30 09:08:22 +0200

    Use SSB specific defaults

--- set_variables.sh
+++ set_variables.sh
@@ -18,9 +18,11 @@
 # The Google Cloud project to use for this tutorial
 export PROJECT_ID="credible-runner-340002"
 
+export PROJECT_NUMBER="123456789012"
+
 # The Compute Engine region to use for running Dataflow jobs and create a
 # temporary storage bucket
-export REGION_ID="us-east1"
+export REGION_ID="europe-north1"
 
 # The Cloud Storage bucket to use as a temporary bucket for Dataflow
 export TEMP_GCS_BUCKET="credible-runner-340002-bucket"
@@ -50,12 +52,25 @@ export KMS_KEY_ID="[key-id]"
 # The JSON file containing the TINK Wrapped data-key to use for encryption
 export WRAPPED_KEY_FILE="[path-to-the-data-encryption-key-file]"
 
+# Name of the Secret Manager key to use for encryption
++export SECRET_MANAGER_KEY_NAME="autodlp_testkey_tinkey_wrapped"
+
+# Name of the VPC to run Dataflow jobs in
+export VPC_NAME="vpc-blah"
+
 ######################################
 #      DON'T MODIFY LINES BELOW      #
 ######################################
 
 # The GCP KMS resource URI
 export MAIN_KMS_KEY_URI="gcp-kms://projects/${PROJECT_ID}/locations/${REGION_ID}/keyRings/${KMS_KEYRING_ID}/cryptoKeys/${KMS_KEY_ID}"
+
+# Secret Manager resource ID that holds the tinkey wrapped key
+export SECRET_MANAGER_KEY_RESOURCE_ID="projects/${PROJECT_NUMBER}/secrets/${SECRET_MANAGER_KEY_NAME}/versions/latest"
+#export SECRET_MANAGER_KEY_RESOURCE_ID="projects/${PROJECT_ID}/secrets/${SECRET_MANAGER_KEY_NAME}/versions/latest"
+
+# The subnetwork to use when running Dataflow jobs
+export DATAFLOW_SUBNETWORK="https://www.googleapis.com/compute/v1/projects/${PROJECT_ID}/regions/${REGION_ID}/subnetworks/${VPC_NAME}"
 
 # The DLP Runner Service account email
 DLP_RUNNER_SERVICE_ACCOUNT_EMAIL="${DLP_RUNNER_SERVICE_ACCOUNT_NAME}@$(echo $PROJECT_ID | awk -F':' '{print $2"."$1}' | sed 's/^\.//').iam.gserviceaccount.com"
